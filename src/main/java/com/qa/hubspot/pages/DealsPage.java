bcf5933a44dbbb04f9cf43826d6a2e45dd3e5c96
Purva Patil <prvpatil@gmail.com>
2019-10-31 21:10:45 +0530

    initialized the contructor

--- src/main/java/com/qa/hubspot/pages/DealsPage.java
+++ src/main/java/com/qa/hubspot/pages/DealsPage.java
@@ -1,10 +1,14 @@
 package com.qa.hubspot.pages;
 
+import org.openqa.selenium.WebDriver;
+
 import com.qa.hubspot.base.BasePage;
 
 public class DealsPage extends BasePage {
+	WebDriver driver;
 
 	// create constructor
 	public DealsPage() {
+		this.driver = driver;
 	}
 }
